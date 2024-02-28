8d75caa5fca7768e92dfa5ca04b30309af3cfddb
silva3265 <silva3265@gmail.com>
2024-02-28 18:06:56 -0300

    Código Limpo: comentários no código

---
+++ src/boas_praticas_clean_code/AposentadoriaTempoContribuicao.java
@@ -0,0 +1,11 @@
+package boas_praticas_clean_code;
+
+public class AposentadoriaTempoContribuicao {
+	
+	 void solicitarAposentadoria(Contribuinte contribuinte) {
+	        if (contribuinte.estaElegivelParaAposentadoria()) {
+	            // TODO implementar aqui
+	        }
+	    }
+
+}
--- src/boas_praticas_clean_code/Fornecedor.java
+++
@@ -1,7 +0,0 @@
-package boas_praticas_clean_code;
-
-public class Fornecedor {
-	
-	 String razaoSocial;
-
-}
