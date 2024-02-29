cc0cd6882a44d73677492b2ed0053efbe806648a
silva3265 <silva3265@gmail.com>
2024-02-29 17:48:35 -0300

    Código Limpo: pensando melhor nos argumentos de métodos

--- src/boas_praticas_clean_code/GatewayPagamento.java
+++
@@ -1,9 +0,0 @@
-package boas_praticas_clean_code;
-
-public class GatewayPagamento {
-
-	static boolean autorizarPagamento(String numeroCartao, double valor) {
-		return true;
-	}
-
-}
---
+++ src/boas_praticas_clean_code/Principal.java
@@ -0,0 +1,21 @@
+package boas_praticas_clean_code;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+        CarrinhoDeCompra carrinho = new CarrinhoDeCompra();
+//        carrinho.adicionarItem("Água", 5, 4);
+
+        Produto produto = new Produto();
+        produto.nome = "Água";
+        produto.precoUnitario = 5;
+
+        carrinho.adicionarItem(produto, 4);
+
+//        produto.alterarStatus(true);
+        produto.ativar();
+        produto.inativar();
+    }
+
+
+}
